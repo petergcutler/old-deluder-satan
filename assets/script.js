76b522638764ce215b974678fd9201bdbd625e68
Matt Ficke <matthew.ficke@gmail.com>
2015-08-25 11:15:36 -0400

    changes

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
       view.render();
     })
   })
+
+  L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([38.90, -77.01], 12);
+
 });
