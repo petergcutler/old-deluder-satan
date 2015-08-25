27e3cbd62d6e5265411d6b8f907687d4f626ae9a
Matt Ficke <matthew.ficke@gmail.com>
2015-08-25 12:00:52 -0400

    add geocoding

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,16 @@ $(document).ready(function() {
   console.log("loading")
   School.fetch().then(function(schools){
     schools.forEach(function(school){
-      var view = new SchoolView(school);
-      view.render();
+      var url = "https://api.mapbox.com/v4/geocode/mapbox.places/" + school.address + ".json?proximity=-77,38.9&access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A"
+      console.log(url)
+      $.getJSON(url).then(function(response){
+        console.log(response)
+      })
+      // var latlng = apicall
+      // map.setMarker(latlng)
+
+      // var view = new SchoolView(school);
+      // view.render();
     })
   })
 
