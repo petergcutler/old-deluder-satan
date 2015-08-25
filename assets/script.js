e2896a9b7178b2329b1956e72d2d1e31be0d9ce6
Matt Ficke <matthew.ficke@gmail.com>
2015-08-25 13:31:20 -0400

    add pins to map

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,25 @@ $(document).ready(function() {
   console.log("loading")
   School.fetch().then(function(schools){
     schools.forEach(function(school){
-      var url = "https://api.mapbox.com/v4/geocode/mapbox.places/" + school.address + ".json?proximity=-77,38.9&access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A"
-      console.log(url)
+      var url = "https://api.mapbox.com/v4/geocode/mapbox.places/" + school.address + ", Washington, District of Columbia.json?proximity=-77,38.9&access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A"
       $.getJSON(url).then(function(response){
-        console.log(response)
+        L.mapbox.featureLayer({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                // lon, lat
+                coordinates: [
+                  response.features[0].center[0],
+                  response.features[0].center[1]
+                ]
+            },
+            properties: {
+                title: school.name,
+                description: school.address,
+                'marker-size': 'small',
+            }
+        }).addTo(map);
       })
-      // var latlng = apicall
-      // map.setMarker(latlng)
-
       // var view = new SchoolView(school);
       // view.render();
     })
