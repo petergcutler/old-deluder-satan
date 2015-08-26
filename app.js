09f3b4186f127831623f4e185b2b069357a5f933
Matt Ficke <matthew.ficke@gmail.com>
2015-08-26 14:25:30 -0400

    add users

--- app.js
+++ app.js
@@ -2,16 +2,68 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var passport = require('passport')
+var Strategy = require('passport-local').Strategy
+var bcrypt = require('bcrypt-nodejs')
 
+app.use(expressSession({
+  secret: 'deluder',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(function(req, res, callback){
+  if(req.user){
+    res.locals.currentUser = req.user.username
+  }
+})
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use("/assets", express.static(path.join(__dirname + "/assets")));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
+
+passport.use(new Strategy(function(username, password, callback){
+  var hashedPass = bcrypt.hashSync(password)
+  User.findOne({
+    where: {
+      username: username
+    }
+  }).then(function(user, err){
+    if (err) { return callback(err); }
+    if (!user) {
+      return callback(null, false);
+    }
+    return callback(null, user);
+  })
+}))
+
+passport.serializeUser(function(user, callback) {
+  callback(null, user.id);
+});
+
+passport.deserializeUser(function(id, callback){
+  User.findById(id).then(function(user){
+    callback(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  if(req.user){
+    res.locals.user = req.user.username
+  }
+  next()
+})
 
 var schoolsController = require('./controllers/schools');
 var usersController = require('./controllers/users');
 var healthReportsController = require('./controllers/healthReports')
 var commentsController = require('./controllers/comments')
+
 
 app.get('/', function(req, res) {
   res.render("index")
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport')
 var DB = require('../config/connection');
 var User = DB.models.User;
 
@@ -12,6 +13,46 @@ router.get('/users', function(req, res) {
   User.findAll().then(function(users){
     res.json(users);
   })
+})
+
+router.get('/signup', function(req, res) {
+  res.render('auth/signup')
+})
+
+router.post('/signup', function(req, res, next) {
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then(function(user){
+    if(!user){
+      User.create({
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password)
+      }).then(function(user){
+        passport.authenticate("local", {
+          failureRedirect: '/signup',
+          successRedirect: '/'
+        })(req, res, next)
+      })
+    } else {
+      res.send("user exists")
+    }
+  })
+})
+
+router.get('/signin', function(req, res){
+  res.render('auth/signin')
+})
+
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  successRedirect: '/'
+}))
+
+router.get('/signout', function(req, res){
+  req.session.destroy()
+  res.redirect('/')
 })
 
 module.exports = router;
