c9a64df591ebf1a513a774b15143e77cef851234
Karl Sakas <donusdonut@Karls-MacBook-Pro.local>
2015-08-26 13:46:14 -0400

    final heroku uplad change

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
+
+  L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A';
+  var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([38.90, -77.01], 12);
+
   console.log("loading")
+
   School.fetch().then(function(schools){
     schools.forEach(function(school){
+
+      var myLayer = L.mapbox.featureLayer().addTo(map);
+
       var url = "https://api.mapbox.com/v4/geocode/mapbox.places/" + school.address + ", Washington, District of Columbia.json?proximity=-77,38.9&access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A"
       $.getJSON(url).then(function(response){
-        L.mapbox.featureLayer({
+      var myLayer = L.mapbox.featureLayer({
+
             type: 'Feature',
             geometry: {
                 type: 'Point',
@@ -18,15 +28,20 @@ $(document).ready(function() {
                 title: school.name,
                 description: school.address,
                 'marker-size': 'small',
+                url: "http://en.wikipedia.org/wiki/Washington,_D.C."
             }
         }).addTo(map);
+    
       })
       // var view = new SchoolView(school);
       // view.render();
     })
+
   })
 
-  L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A';
-  var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([38.90, -77.01], 12);
+
+
+
+
+
 });
--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    logging: true
+    logging: true //false
   });
 } else {
   sequelize = new Sequelize('postgres:///deluder');
