f953da3c3bfb22305224f67654cc920d2df29ba0
Matt Ficke <matthew.ficke@gmail.com>
2015-08-27 15:05:30 -0400

    remove extra method

--- assets/views/schoolView.js
+++ assets/views/schoolView.js
@@ -23,23 +23,6 @@ SchoolView.prototype = {
 
     self.$el.html(self.schoolTemplate(self.school));
 
-  },
-  schoolTemplate: function(school) {
-    var html = $("<div>");
-    // html.append("<p>School id is</p>" + this.school.id + "<p>and the name is</p>" + this.school.name);
-    // html.append("<div class='schoolInfo'></div>");
-    // html.append("<p>" + school.address + "</p>");
-    return(html);
   }
-  // toggleSchool: function(schoolDiv) {
-  //   var self = this;
-  //   if(schoolDiv.children().length ===0){
-  //     this.school.fetchHealthReport().then(function(report){
-  //       schoolDiv.append("<p>" + self.school.address + "</p>")
-  //       schoolDiv.append("<a href='" + report.reportUrl + "'>School Cafeteria Helath Risk Category: " + report.riskCategory + "</a>")
-  //       console.log(report);
-  //     })
-  //   }
-  //   schoolDiv.toggle();
-  // }
+
 }
