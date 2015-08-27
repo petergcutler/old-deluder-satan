c745576ae9ef3a3a12c739299b6e18b606a6614d
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-08-27 09:43:46 -0400

    render school info via JSON instead of mapbox

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log("loading")
   School.fetch().then(function(schools){
     schools.forEach(function(school){
       var url = "https://api.mapbox.com/v4/geocode/mapbox.places/" + school.address + ", Washington, District of Columbia.json?proximity=-77,38.9&access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A"
@@ -17,22 +16,30 @@ $(document).ready(function() {
             properties: {
                 title: school.name,
                 description: school.address,
+                identity: school.id,
                 testdisplay: "this is a test",
                 'marker-size': 'small',
             }
         })
-        // .bindPopup('<button class="trigger">Say hi</button>')
         .addTo(map);
 
         schoolMapList.on('click', function(e){
-          console.log(e.layer.feature.properties.title)
-          $(".one-school-name").html(e.layer.feature.properties.title)
-          $(".one-school-address").html(e.layer.feature.properties.description)
-          console.log(e.layer.feature.properties.testdisplay)
+          var schoolId = e.layer.feature.properties.identity
+          var url = "/schools/" + schoolId
+          var schoolRequest = $.getJSON(url).then(function(response){
+            schoolname = response.name
+            schoolid = response.id
+            schooladdress = response.address
+          })
+          var view = new SchoolView(schoolRequest)
+          console.log(schoolname + schoolid + schooladdress)
+
+          // console.log(e.layer.feature.properties.title)
+          // console.log(e.layer.feature.properties.identity)
+          $(".one-school-name").html(schoolname)
+          $(".one-school-address").html(schooladdress)
         })
       })
-      // var view = new SchoolView(school);
-      // view.render();
     })
   })
 
